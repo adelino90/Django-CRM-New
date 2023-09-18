b0ae413be2aaaf16e9afe7518d9249121205f3de
Adelino Justo <adelinojusto911@gmail.com>
2023-09-18 21:34:46 +0800

    Model and Bootstrap Table

--- website/views.py
+++ website/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 def home (request):
-    #check to see if logging in
+    
+    records = Record.objects.all()
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -17,7 +20,7 @@ def home (request):
             messages.success(request, "There was an error logging in please try again")
             return redirect('home')
     else:
-        return render(request, 'home.html',{})
+        return render(request, 'home.html',{'records':records})
 
 
 
@@ -27,4 +30,19 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request, 'register.html',{})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request,username=username, password=password)
+            login(request,user)
+            messages.success(request,"You have successfuly register")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html',{'form':form})
+
+    return render(request, 'register.html',{'form':form})
